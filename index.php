236696c836e738ebb9d00445f85562e052c0908f
José ramos <nettorammos@hotmail.com>
2019-10-31 13:03:00 -0300

    Páginas Estilizadas

--- index.php
+++ index.php
@@ -43,13 +43,13 @@
       <td>30.00</td>
     </tr>
     <tr>
-      <th scope="row">1</th>
+      <th scope="row">2</th>
       <td>Camiseta PHP</td>
       <td>Camisetas</td>
       <td>30.00</td>
     </tr>
     <tr>
-      <th scope="row">1</th>
+      <th scope="row">3</th>
       <td>Camiseta React</td>
       <td>Camisetas</td>
       <td>30.00</td>
@@ -60,6 +60,41 @@
 </div>
 <div class="new-produto">
     <h2 class="text-center">Adicionar Produto </h2>
+    <div class="form-all mt-4">
+        <form action="produto.php" method="post">
+            <div class="form-group">
+                <label for="nome">Nome</label>
+                <input type="text" id="nome" class="form-control"  name="nome" placeholder="Nome do Produto">
+            </div>
+            <div class="form-group">
+              <label for="categoria">Categoria </label>
+                <select name="categoria" id="categoria"  class="form-control">
+                  <option value="1">Camisetas</option>
+                  <option value="2">Adesivos</option>
+                  <option value="3">Copos</option>
+                </select>
+            </div>
+            <div class="form-group">
+              <label for="descricao">Descrição</label>
+              <textarea class="form-control" id="descricao" name="descricao" rows="3"></textarea>
+            </div>
+
+            <div class="form-group">
+                <label for="quantidade">Quantidade</label>
+                <input type="text" id="quantidade" class="form-control"  name="quantidade" placeholder="quantidade do Produto">
+            </div>
+
+            <div class="form-group">
+                <label for="preco">Preço</label>
+                <input type="text" id="preco" class="form-control"  name="preco" placeholder="Preço do Produto">
+            </div>
+            <div class="form-group">
+              <label for="file">Imagem do produto</label>
+              <input type="file" class="form-control-file" name="img-produto" id="file">
+            </div>
+            <button class="btn btn-my">Cadastrar</button>
+        </form>
+    </div>
 </div>
 </main>
 </body>

b2a962d16074928eaa52314ded050a83d97a8e39
José ramos <nettorammos@hotmail.com>
2019-11-04 17:33:15 -0300

    Tirando a div e deixando o layout completo

--- produto.php
+++ produto.php
@@ -4,27 +4,25 @@
 ?>
 <main class="d-flex box">
 
-        <div class="col-6">
+        <div class="col-6 ">
         <a href="index.php" class="btn mt-3 mb-3">Voltar para lista de produtos</a>
         <img src="<?= $produto["img"] ?>" class="img-fluid" alt="">
         </div>
-        <div class="col-6">
-            <h2 class="mt-5"><?= $produto["nome"] ?></h2>
+        <div class="col-6 mt-5">
+            <h2 class="mt-3 text-white"><?= $produto["nome"] ?></h2>
             
-            <h4 class="mt-5">Categoria</h4>
+            <h4 class="mt-3">Categoria</h4>
             <label><?= $produto["categoria"] ?></label>
             
-            <h4 class="mt-5">Descrição</h4>
+            <h4 class="mt-3">Descrição</h4>
             <label><?= $produto["descricao"] ?></label>
 
-            <div class="row mt-5 justify-content-around">
             <h4>Quantidade</h4>
             <label><?= $produto["quantidade"] ?></label>
 
             <h4>Preço</h4>
             <label><?= $produto["preco"] ?></label>
             
-            </div>
         </div>
 </main>
 <?php require('footer.php') ?>
